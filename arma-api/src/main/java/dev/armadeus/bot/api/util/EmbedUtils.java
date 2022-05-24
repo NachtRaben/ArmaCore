510a9fc016ea65af593d906b07092bf042fb6a18
NachtRaben <brandon@armadeus.dev>
2022-05-24 13:06:29 -0400

    Adjust embed utils
    Signed-off-by: NachtRaben <brandon@armadeus.dev>

--- arma-api/src/main/java/dev/armadeus/bot/api/util/EmbedUtils.java
+++ arma-api/src/main/java/dev/armadeus/bot/api/util/EmbedUtils.java
@@ -2,14 +2,25 @@ package dev.armadeus.bot.api.util;
 
 import dev.armadeus.bot.api.command.DiscordCommandIssuer;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.User;
 
 public class EmbedUtils {
 
     public static EmbedBuilder newBuilder(DiscordCommandIssuer user) {
+        if (user.isFromGuild())
+            return newBuilder(user.getMember());
+        else
+            return newBuilder(user.getUser());
+    }
+
+    public static EmbedBuilder newBuilder(User user) {
+        return new EmbedBuilder();
+    }
+
+    public static EmbedBuilder newBuilder(Member user) {
         EmbedBuilder builder = new EmbedBuilder();
-        if (user.isFromGuild()) {
-            builder.setColor(user.getGuild().getSelfMember().getRoles().get(0).getColor());
-        }
+        builder.setColor(user.getGuild().getSelfMember().getRoles().get(0).getColor());
         return builder;
     }
 }
