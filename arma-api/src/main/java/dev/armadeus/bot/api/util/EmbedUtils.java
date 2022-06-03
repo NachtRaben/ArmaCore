18e1d42715188dabab022039da64410c08598adf
NachtRaben <brandon@armadeus.dev>
2022-06-02 22:19:47 -0400

    Hotfix no roles
    Signed-off-by: NachtRaben <brandon@armadeus.dev>

--- arma-api/src/main/java/dev/armadeus/bot/api/util/EmbedUtils.java
+++ arma-api/src/main/java/dev/armadeus/bot/api/util/EmbedUtils.java
@@ -3,7 +3,11 @@ package dev.armadeus.bot.api.util;
 import dev.armadeus.bot.api.command.DiscordCommandIssuer;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.User;
+
+import java.awt.*;
+import java.util.List;
 
 public class EmbedUtils {
 
@@ -20,7 +24,8 @@ public class EmbedUtils {
 
     public static EmbedBuilder newBuilder(Member user) {
         EmbedBuilder builder = new EmbedBuilder();
-        builder.setColor(user.getGuild().getSelfMember().getRoles().get(0).getColor());
+        List<Role> roles = user.getGuild().getSelfMember().getRoles();
+        builder.setColor(!roles.isEmpty() ? roles.get(0).getColor() : Color.BLUE);
         return builder;
     }
 }
